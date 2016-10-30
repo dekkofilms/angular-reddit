b367ec1895ea0881321ad0fe3a9df11c50e3e90f
Taylor King <dekkofilms@gmail.com>
2016-10-30 16:22:50 -0500

    new post form appears when button is clicked, and pushes in posts array

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = angular.module('reddit', []);
 
 app.controller('posts', ['$scope', ($scope) => {
   $scope.view = {};
+  $scope.view.newpost = false;
   $scope.view.posts = [
     {
       title : 'This is my First Post!',
@@ -12,9 +13,55 @@ app.controller('posts', ['$scope', ($scope) => {
       comments : ['This is an awesome lorem idea', 'Why do you even try?'],
       count: 0,
     },
+    {
+      title : 'This is my Second Post!',
+      content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      imageLink : 'https://twistedsifter.files.wordpress.com/2012/05/funny-alpacas-with-awesome-amazing-hilarious-hair-5.jpg',
+      date : new Date (),
+      author : 'Jesse Witt',
+      comments : ['Derka derka derka'],
+      count: 0,
+    },
+    {
+      title : 'This is my Third Post!',
+      content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      imageLink : 'https://twistedsifter.files.wordpress.com/2012/05/funny-alpacas-with-awesome-amazing-hilarious-hair-4.jpg',
+      date : new Date (),
+      author : 'Andrew Soderlind',
+      comments : [],
+      count: 0,
+    },
   ]
 
-  $scope.upvote = (id) => {
-    console.log(id);
+  $scope.vote = (id, flag) => {
+    if (flag === "up") {
+      $scope.view.posts[id].count++;
+    }
+
+    if (flag === "down") {
+      $scope.view.posts[id].count--;
+    }
   }
+
+  $scope.submit = () => {
+    //defining the new post submission
+    let newPost = {};
+    newPost.title = $scope.title;
+    newPost.content = $scope.description;
+    newPost.imageLink = $scope.imageLink;
+    newPost.date = new Date ();
+    newPost.author = $scope.author;
+    newPost.comments = [];
+    newPost.count = 0;
+
+    //pushing into my posts array
+    $scope.view.posts.push(newPost)
+    //
+    //clear form
+    $scope.title = '';
+    $scope.description = '';
+    $scope.imageLink = '';
+    $scope.author = '';
+  }
+
 }]);
