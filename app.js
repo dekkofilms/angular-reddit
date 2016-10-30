52656be4d7e17d7335f647fd834385a86ffdce9f
Taylor King <dekkofilms@gmail.com>
2016-10-30 16:49:17 -0500

    posts dynamically order themselves by vote counts

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = angular.module('reddit', []);
 app.controller('posts', ['$scope', ($scope) => {
   $scope.view = {};
   $scope.view.newpost = false;
+  $scope.view.idCount = 2;
   $scope.view.posts = [
     {
+      id : 0,
       title : 'This is my First Post!',
       content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       imageLink : 'https://twistedsifter.files.wordpress.com/2012/05/funny-alpacas-with-awesome-amazing-hilarious-hair-6.jpg',
@@ -14,6 +16,7 @@ app.controller('posts', ['$scope', ($scope) => {
       count: 0,
     },
     {
+      id : 1,
       title : 'This is my Second Post!',
       content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       imageLink : 'https://twistedsifter.files.wordpress.com/2012/05/funny-alpacas-with-awesome-amazing-hilarious-hair-5.jpg',
@@ -23,6 +26,7 @@ app.controller('posts', ['$scope', ($scope) => {
       count: 0,
     },
     {
+      id : 2,
       title : 'This is my Third Post!',
       content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       imageLink : 'https://twistedsifter.files.wordpress.com/2012/05/funny-alpacas-with-awesome-amazing-hilarious-hair-4.jpg',
@@ -44,8 +48,11 @@ app.controller('posts', ['$scope', ($scope) => {
   }
 
   $scope.submit = () => {
+    $scope.view.idCount++;
+
     //defining the new post submission
     let newPost = {};
+    newPost.id = $scope.view.idCount;
     newPost.title = $scope.title;
     newPost.content = $scope.description;
     newPost.imageLink = $scope.imageLink;
